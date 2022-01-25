0699e5680484ee20ff8b19d44ff0553ee20663af
Nicolas <nicolas.ramirez.28@hotmail.com>
2022-01-25 04:40:10 -0300

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -20,20 +20,29 @@ var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload, done) => {
-        console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
-            if (err) {
-                return done(err, false);
-            }
-            else if (user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
-            }
-        });
-    }));
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT payload: ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        else if (user) {
+            return done(null, user);
+        }
+        else {
+            return done(null, false);
+        }
+    });
+}));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyAdmin = (req, res, next)=>{
+    if (req.user.admin) {
+        return next();
+    }
+    else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        next(err);
+    }
+};
